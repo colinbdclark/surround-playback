773c6c7bbc59572a8e36aa28d7bab67bc007cb33
Colin Clark <colinbdclark@gmail.com>
2014-12-21 11:43:24 -0500

    Adds support for any channel count available.

--- js/webaudio.js
+++ js/webaudio.js
@@ -1,27 +1,25 @@
-/* 
+/*
  *	This is our sound object
  */
 function Sound(source, level) {
-	if (!window.audioContext) {
-		audioContext = new AudioContext();
+	var constructorName = window.AudioContext || window.webkitAudioContext;
+	if (!constructorName) {
+		throw new Error("Your browser doesn't currently support the Web Audio API.");
 	}
-	// test to see how many hardware channels we can output to
-	// if it's 6 or larger, we can play a 5.1 audio stream!
-	if (audioContext.destination.maxChannelCount >= 6) {
-		audioContext.destination.channelCount = 6;
-	}
-	// otherwise, let's down-mix to 2.0
-	else {
-		audioContext.destination.channelCount = 2;
-	}
-	audioContext.destination.channelCountMode = "explicit";
-	audioContext.destination.channelInterpretation = "discrete";
+
+	this.audioContext = new constructorName();
+
+	// Set the audio context to use as many channels as possible.
+	// This will allow us to hear whether the browser supports multiple channels.
+	this.audioContext.destination.channelCount = this.audioContext.destination.maxChannelCount;
+	this.audioContext.destination.channelCountMode = "explicit";
+	this.audioContext.destination.channelInterpretation = "discrete";
 
 	var that = this;
 	that.source = source;
 	that.buffer = null;
 	that.isLoaded = false;
-	that.volume = audioContext.createGain();
+	that.volume = this.audioContext.createGain();
 	if (!level) {
 		that.volume.gain.value = 1;
 	}
@@ -30,7 +28,7 @@ function Sound(source, level) {
 	}
 
 	var getSound = Base64Binary.decodeArrayBuffer(that.source);
-	audioContext.decodeAudioData(getSound, function(buffer) {
+	this.audioContext.decodeAudioData(getSound, function(buffer) {
 			that.buffer = buffer;
 			that.isLoaded = true;
 		}
@@ -40,29 +38,31 @@ function Sound(source, level) {
 // our play function
 // playbackRate allows for changing the speed of playback, mainly for SFX
 Sound.prototype.play = function (playbackRate) {
+	var playSound;
+
 	if (!playbackRate) {
 		playbackRate = 1;
 	}
 	// if the sound is loaded
 	if (this.isLoaded === true) {
-		var playSound = audioContext.createBufferSource();
+		playSound = this.audioContext.createBufferSource();
 		playSound.buffer = this.buffer;
 		playSound.connect(this.volume);
 		playSound.playbackRate.value = playbackRate;
-		this.volume.connect(audioContext.destination);
+		this.volume.connect(this.audioContext.destination);
 		playSound.start(0);
 	}
 
 	// return the context so we can work with it later!
 	return playSound;
-}
+};
 
 Sound.prototype.setVolume = function (level) {
 	this.volume.gain.value = level;
-}
+};
 
-// pass the returned playSound context 
+// pass the returned playSound context
 // (from Sound.prototype.play) in order to stop sound playback
 Sound.prototype.killSound = function (context) {
 	context.stop(0);
-}
+};
